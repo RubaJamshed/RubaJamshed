8a4aeeeb0a802daedb1e7aa85a47215d12e86240
RubaJamshed <rubajamshed@gmail.com>
2024-11-10 23:23:10 +0500

    Add files via upload

---
+++ pflab95.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+void reverseString(char str[]);
+
+int main()
+{
+    char str[100];
+    printf("Enter a string to reverse: ");
+    scanf("%s", str);
+    
+    
+    reverseString(str);
+    
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
+
+
+void reverseString(char str[]) 
+{
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
